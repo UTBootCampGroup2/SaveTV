c322df7d44c26f8e705034b05fab680c086990c8
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-21 14:30:33 -0500

    add and call function for tvmaze api

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,7 +1,28 @@
 // global variables
 
 // Function for searching the series name entered in the input form element
+var getSeries = function(seriesName) {
 
+    var apiUrl = "http://api.tvmaze.com/singlesearch/shows?q=" + seriesName;
+  
+    fetch(apiUrl)
+    .then(function(response) {
+      // request was successful
+      if (response.ok) {
+        //console.log(response);
+        response.json().then(function(data) {
+          //console.log(data);
+          displaySeriesdata(data);
+        });
+      } else {
+        alert('Error: ' + response.statusText);
+      }
+    })
+    .catch(function(error) {
+      alert('Unable to connect');
+    });
+  
+  };
 // Function for displaying the serached series information
 
 // Function for getting rating
