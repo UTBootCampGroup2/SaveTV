01758d44cbbbc601f179dbfa6ce17005785d662e
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-21 14:28:24 -0500

    add pseudocodes

---
+++ assets/js/search-results.js
@@ -0,0 +1,12 @@
+// global variables
+
+// Function for searching the series name entered in the input form element
+
+// Function for displaying the serached series information
+
+// Function for getting rating
+
+// Function to display rating
+
+//call search
+
