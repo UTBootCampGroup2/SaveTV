321b4386b1d737aaaf45e9fb0f79ae9c89461e22
BrianCKWang <brian.ck.wang@gmail.com>
2021-01-22 15:18:52 -0500

    combine js code from Rose and Brandon

--- assets/js/search-results.js
+++ assets/js/search-results.js
@@ -1,3 +1,110 @@
+
+// ************************ From Brandon ************************//
+// fetching TV Maze API 
+function searchShow(query) {
+  var url = `http://api.tvmaze.com/search/shows?q=${query}`;
+  console.log(url);
+
+  fetch(url)
+  // .then(response => response.json())
+  .then(function(response){
+    if(response.ok){
+      return response.json();
+    }
+    else{
+      console.log('Unable to connect to tvmaze');
+    }
+  })
+  .then((jsonData) => {
+    
+    // gets specific string from array
+    if(jsonData != null){
+      var movieInfoList =  jsonData.map((element) => ({
+        movieId: element.show.id, 
+        imgLink: element.show.image.medium
+      }));
+    }
+
+    renderResults(movieInfoList);
+  
+    // displays error message in html
+    document.getElementById("errorMessage").innerHTML = "";
+      
+  })
+  .catch((error) => {
+      document.getElementById("errorMessage").innerHTML = error;
+  });
+}
+
+document.onclick = function (event) {
+  event.preventDefault();
+  var target = event.target;
+
+  if (target.tagName.toLowerCase() === 'img') {
+      console.log("img click test") //works
+      console.log('event', event);
+
+      var movieId = target.id;
+      console.log(movieId);
+      displayModal();
+  }
+}
+
+// lists JSON objects on HTML 
+function renderResults(movieInfoList) {
+ var list = document.getElementById("resultsList");
+ list.innerHTML="";
+ movieInfoList.forEach(movieInfo => {
+
+  // turns img string to img
+     showImage(movieInfo);
+ });
+}
+
+function showImage(movieInfo) {
+  // creates img
+  var img = document.createElement("img")
+  // console.log(movieInfo);
+  img.id = movieInfo.movieId;
+  img.src = movieInfo.imgLink;
+  
+  //images will populate in <section>
+  document.querySelector('section').appendChild(img);
+
+}
+
+function displayModal() {
+  var modalBox = document.querySelector(".modal-box");
+  modalBox.classList.add("activeInfo");
+
+  var exitBtn = modalBox.querySelector(".quit");
+  exitBtn.onclick = ()=>{
+    modalBox.classList.remove("activeInfo");
+  }
+}
+
+let searchTimeout = 0;
+
+// automatically starts searching just from input of search bar
+window.onload = () => {
+  var searchField = document.getElementById("findlocate")
+  searchField.onkeyup = (event) => {
+
+      clearTimeout(searchTimeout);
+
+      if(searchField.value.trim().length === 0) {
+          return;
+      }
+
+      searchTimeout = setTimeout(() => {
+        console.log(searchField.value.trim());
+          searchShow(searchField.value.trim());
+      }, 250);
+      
+  }
+}
+
+// ************************ From Rose ************************//
 // global variables
 var searchShowEl = document.querySelector("#search-show");
 var searchInputEl = document.querySelector('#findlocate');
@@ -30,16 +137,17 @@ var searchSubmitHandler = function(event) {
 // Function for searching the series name entered in the input form element
 var getSeries = function(seriesName) {
 
-    var apiUrl = "http://api.tvmaze.com/singlesearch/shows?q=" + seriesName;
-  
+    // var apiUrl = "http://api.tvmaze.com/singlesearch/shows?q=" + seriesName; // From Rose
+    var apiUrl = "http://api.tvmaze.com/search/shows?q=" + seriesName;          // From Brandon
     fetch(apiUrl)
     .then(function(response) {
       // request was successful
       if (response.ok) {
         //console.log(response);
         response.json().then(function(data) {
-          //console.log(data);
-          displaySeriesdata(data);
+          console.log("In getSeries: ");
+          // console.log(data);
+          // displaySeriesdata(data);
         });
       } else {
         alert('Error: ' + response.statusText);
@@ -50,10 +158,12 @@ var getSeries = function(seriesName) {
     });
   
 };
+
 // Function for displaying the serached series information
 var displaySeriesdata = function(series) {
   
-
+  console.log(series);
+  if(series != null){
     // create section for holding series data
     var seriesDataEl = document.createElement('div');
     seriesDataEl.className = 'series-data';
@@ -61,13 +171,13 @@ var displaySeriesdata = function(series) {
     resultContainerEl.appendChild(seriesDataEl);
 
     // display Image
-    console.log(series.image.medium);
-    var imageEl = document.createElement('img');
-    imageEl.className = ('search-image')
-    imageEl.setAttribute("id", 'search-image');
-    imageEl.setAttribute("src", series.image.medium);
-    seriesDataEl.appendChild(imageEl);
-  
+    // console.log(series.image.medium);
+    // var imageEl = document.createElement('img');
+    // imageEl.className = ('search-image')
+    // imageEl.setAttribute("id", 'search-image');
+    // imageEl.setAttribute("src", series.image.medium);
+    // seriesDataEl.appendChild(imageEl);
+
     // display name of the series
     console.log(series.name);
     var nameEl = document.createElement('p');
@@ -85,16 +195,16 @@ var displaySeriesdata = function(series) {
     websiteEl.setAttribute("href", series.officialSite);
     websiteEl.setAttribute("target", '_blank');
     seriesDataEl.appendChild(websiteEl);
-    
+
     // display status or schedule
     if(series.status != 'Ended') {
       //console.log(series.schedule.time + series.schedule.days);
-  
+
       var scheduleEl = document.createElement('p');
       scheduleEl.className = ('search-schedule')
       scheduleEl.setAttribute("id", 'search-schedule');
       scheduleEl.textContent = series.schedule.time + " "+ series.schedule.days;
-     
+    
       seriesDataEl.appendChild(scheduleEl);
     }
     else {
@@ -105,7 +215,7 @@ var displaySeriesdata = function(series) {
       statusEl.textContent = "Status: " + series.status;
       seriesDataEl.appendChild(statusEl);
     }
-  
+
     // add save button
     var saveButtonEl = document.createElement('button');
     saveButtonEl.className = 'save-button';
@@ -114,7 +224,7 @@ var displaySeriesdata = function(series) {
     saveButtonEl.innerHTML = "<i class='fa fa-search'></i>"
     saveButtonEl.textContent = "Save";
     seriesDataEl.appendChild(saveButtonEl);
-  
+
     // display summary
     var summaryEl = document.createElement('div');
     summaryEl.className = 'search-summary';
@@ -124,7 +234,12 @@ var displaySeriesdata = function(series) {
 
     // call function to get rating
     seriesRating(series.externals.imdb);
-  };
+  }
+  else{
+    console.log("displaySeriesdata input is null");
+  }
+    
+};
 
 // Function for getting rating
 var seriesRating = function(id) {
@@ -177,6 +292,4 @@ var displayRating = function(rating) {
 };
 
 // add event listener for search
-searchFormEl.addEventListener('submit', searchSubmitHandler);
-
-
+// searchFormEl.addEventListener('submit', searchSubmitHandler);
