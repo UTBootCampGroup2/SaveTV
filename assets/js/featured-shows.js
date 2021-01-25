1ef6d0ac643f23acd5db18ad6526fdbfc1c2a2bc
rosefrancis-tech <32376285+rosefrancis-tech@users.noreply.github.com>
2021-01-25 17:44:12 -0500

    display shows in six all sections

--- assets/js/featured-shows.js
+++ assets/js/featured-shows.js
@@ -31,7 +31,6 @@ var featuredwebEl5 = document.getElementById('web5');
 var featuredDetailsEl5 = document.getElementById('details5');
 var featuredShowName5 = null;
 
-
 // function to fetch shows for the day
 var todaysShows = function () {
 
@@ -77,10 +76,72 @@ var displayFeaturedShows = function(featuredShows) {
   featuredShowName0 = featuredShows[arr[0]].show.name;
   featuredImageEl0.setAttribute('alt', "Poster of " + featuredShowName0);
   featuredwebEl0.setAttribute('href', featuredShows[arr[0]].show.officialSite);
-   
+  
+  // Second featured show
+  var hasImage1 = featuredShows[arr[1]].show.image;
+  // reload if image is not present
+  if(hasImage1 === null){
+    location.reload();
+  }
+  featuredImageEl1.removeAttribute('src');
+  featuredImageEl1.setAttribute('src', hasImage1.medium);
+  featuredImageEl1.removeAttribute('alt');
+  featuredShowName1 = featuredShows[arr[1]].show.name;
+  featuredImageEl1.setAttribute('alt', "Poster of " + featuredShowName1);
+  featuredwebEl1.setAttribute('href', featuredShows[arr[1]].show.officialSite);
 
+  // Third featured show
+  var hasImage2 = featuredShows[arr[2]].show.image;
+  // reload if image is not present
+  if(hasImage2 === null){
+    location.reload();
+  }
+  featuredImageEl2.removeAttribute('src');
+  featuredImageEl2.setAttribute('src', hasImage2.medium);
+  featuredImageEl2.removeAttribute('alt');
+  featuredShowName2 = featuredShows[arr[2]].show.name;
+  featuredImageEl2.setAttribute('alt', "Poster of " + featuredShowName2);
+  featuredwebEl2.setAttribute('href', featuredShows[arr[2]].show.officialSite);
 
-  };
+  // Fourth featured show
+  var hasImage3 = featuredShows[arr[3]].show.image;
+  // reload if image is not present
+  if(hasImage3 === null){
+    location.reload();
+  }
+  featuredImageEl3.removeAttribute('src');
+  featuredImageEl3.setAttribute('src', hasImage3.medium);
+  featuredImageEl3.removeAttribute('alt');
+  featuredShowName3 = featuredShows[arr[3]].show.name;
+  featuredImageEl3.setAttribute('alt', "Poster of " + featuredShowName3);
+  featuredwebEl3.setAttribute('href', featuredShows[arr[3]].show.officialSite);
+
+  // Fifth featured show
+  var hasImage4 = featuredShows[arr[4]].show.image;
+  // reload if image is not present
+  if(hasImage4 === null){
+    location.reload();
+  }
+  featuredImageEl4.removeAttribute('src');
+  featuredImageEl4.setAttribute('src', hasImage4.medium);
+  featuredImageEl4.removeAttribute('alt');
+  featuredShowName4 = featuredShows[arr[4]].show.name;
+  featuredImageEl4.setAttribute('alt', "Poster of " + featuredShowName4);
+  featuredwebEl4.setAttribute('href', featuredShows[arr[4]].show.officialSite);
+
+  // Sixth featured show
+  var hasImage5 = featuredShows[arr[5]].show.image;
+  // reload if image is not present
+  if(hasImage5 === null){
+    location.reload();
+  }
+  featuredImageEl5.removeAttribute('src');
+  featuredImageEl5.setAttribute('src', hasImage5.medium);
+  featuredImageEl5.removeAttribute('alt');
+  featuredShowName5 = featuredShows[arr[5]].show.name;
+  featuredImageEl5.setAttribute('alt', "Poster of " + featuredShowName5);
+  featuredwebEl5.setAttribute('href', featuredShows[arr[5]].show.officialSite);
+};
 
 // Functions for displaying details in modal
 var showDetails0 = function() {
@@ -110,4 +171,4 @@ featuredDetailsEl3.addEventListener('click', showDetails3);
 featuredDetailsEl4.addEventListener('click', showDetails4);
 featuredDetailsEl5.addEventListener('click', showDetails5);
 
-  todaysShows();
+todaysShows();
